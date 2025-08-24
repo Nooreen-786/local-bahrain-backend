f16807b2b85708ac616443f8723e66ae0ab89eb9
User Name <nooreenkhatoon786@gmail.com>
2025-08-24 17:29:59 +0300

    Fix CORS issues: added 127.0.0.1:5174 and moved middleware

--- server.js
+++ server.js
@@ -9,19 +9,16 @@ dotenv.config();
 const app = express();
 
 
-app.use(express.json());
-
-
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:5173',
+  'http://127.0.0.1:5174',  
   'https://local-bahrain-frontend.vercel.app'
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin) return callback(null, true); 
-
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -32,8 +29,12 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
+
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+
+
+app.use(express.json());
 
 
 if (!process.env.MONGO_URI) {
@@ -41,31 +42,32 @@ if (!process.env.MONGO_URI) {
   process.exit(1);
 }
 
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log(' MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+.catch(err => console.error(' MongoDB connection error:', err));
 
 
 const authRoutes = require('./routes/authRoutes');
 const placeRoutes = require('./routes/placeRoutes');
 const restaurantRoutes = require('./routes/RestaurantRoutes');
 
+
 app.use('/api/auth', authRoutes);
 app.use('/api/places', placeRoutes);
 app.use('/api/restaurants', restaurantRoutes);
 
- {
- 
-  app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
-}
+
+app.get('/', (req, res) => {
+  res.send(' API is running...');
+});
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(` Server running on port ${PORT}`);
 });
+
