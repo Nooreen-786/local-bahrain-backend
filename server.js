2ab1b5d292a94d30a71c07eaada932aed9d0647f
User Name <nooreenkhatoon786@gmail.com>
2025-08-24 03:29:45 +0300

    Remove client/build static serving for production deployment

--- server.js
+++ server.js
@@ -57,15 +57,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/places', placeRoutes);
 app.use('/api/restaurants', restaurantRoutes);
 
-
-if (process.env.NODE_ENV === 'production') {
-  const clientBuildPath = path.join(__dirname, 'client', 'build');
-  app.use(express.static(clientBuildPath));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(clientBuildPath, 'index.html'));
-  });
-} else {
+ {
  
   app.get('/', (req, res) => {
     res.send('API is running...');
